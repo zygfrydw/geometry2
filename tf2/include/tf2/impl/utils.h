zygfrydw/geometry2

commit cf612728c77f6c671e1534556f16b75cb352b53c

Deprecate tf2_geometry_msgs.h (#418)
Move it to tf2_geometry_msgs.hpp instead.

Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

tf2/include/tf2/impl/utils.h
@@ -15,7 +15,6 @@
 #ifndef TF2__IMPL__UTILS_H_
 #define TF2__IMPL__UTILS_H_
 
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2/transform_datatypes.h>
 #include <tf2/LinearMath/Quaternion.h>
 

test_tf2/test/buffer_core_test.cpp
@@ -48,7 +48,7 @@
 #include <tf2/exceptions.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/time.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer_interface.h>
 
 #include "permuter.hpp"

test_tf2/test/test_buffer_client.cpp
@@ -38,7 +38,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_bullet/tf2_bullet.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_kdl/tf2_kdl.hpp>
 #include <tf2_ros/buffer_client.h>
 

test_tf2/test/test_convert.cpp
@@ -38,7 +38,7 @@
 #include <tf2/convert.h>
 #include <tf2_kdl/tf2_kdl.hpp>
 #include <tf2_bullet/tf2_bullet.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 TEST(tf2Convert, kdlToBullet)
 {

test_tf2/test/test_utils.cpp
@@ -17,7 +17,7 @@
 #include <geometry_msgs/msg/quaternion.hpp>
 #include <geometry_msgs/msg/quaternion_stamped.hpp>
 #include <geometry_msgs/msg/transform.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/utils.h>
 #include <tf2_kdl/tf2_kdl.hpp>

tf2_geometry_msgs/mainpage.dox
@@ -11,9 +11,9 @@ such as Vector, Point, Pose, Transform, Quaternion, etc.
 See the <A HREF="http://wiki.ros.org/tf2/Tutorials/Migration/DataConversions">Conversions overview</A>
 wiki page for more information about datatype conversion in tf2.
 
-\section codeapi Code API 
+\section codeapi Code API
 
-This library consists of one header only, tf2_geometry_msgs/tf2_geometry_msgs.h, which consists mostly of
+This library consists of one header only, tf2_geometry_msgs/tf2_geometry_msgs.hpp, which consists mostly of
 specializations of template functions defined in tf2/convert.h.
 
 */

commit 31061c98e4c449f33637b7029791a5411b3017e0

Make sure to include needed parts for the utilities.
It turns out that commit cf612728c77f6c671e1534556f16b75cb352b53c
broke some compilation.  However, we don't want to add
it back as it is was an undeclared circular dependency between tf2 and
tf2_geometry_msgs.  Instead, do a couple of things:

1. Include geometry_msgs headers in tf2/include/tf2/impl/utils.h.  This
   ensures that the headers are included where they are needed, and
   geometry_msgs is already a dependency of the tf2 package.
2. Include tf2/convert.h in tf2/include/tf2/utils.h.  This ensures
   that the toMsg() and fromMsg() templates are available when
   generating the code, which ensures that the compiler will go
   looking for a specialization.

Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

tf2/include/tf2/impl/utils.h
@@ -15,6 +15,8 @@
 #ifndef TF2__IMPL__UTILS_H_
 #define TF2__IMPL__UTILS_H_
 
+#include <geometry_msgs/msg/quaternion.hpp>
+#include <geometry_msgs/msg/quaternion_stamped.hpp>
 #include <tf2/transform_datatypes.h>
 #include <tf2/LinearMath/Quaternion.h>
 

tf2/include/tf2/utils.h
@@ -15,10 +15,10 @@
 #ifndef TF2__UTILS_H_
 #define TF2__UTILS_H_
 
+#include <tf2/convert.h>
 #include <tf2/LinearMath/Transform.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/impl/utils.h>
-#include <tf2/visibility_control.h>
 
 namespace tf2
 {